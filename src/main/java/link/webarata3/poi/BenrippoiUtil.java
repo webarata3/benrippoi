509c1da9158b9de5d4f34e3b06db3e55ebd00f97
webarata3 <webarata3@gmail.com>
2017-05-20 17:21:19 +0900

    コメントの記述

--- src/main/java/link/webarata3/poi/BenrippoiUtil.java
+++ src/main/java/link/webarata3/poi/BenrippoiUtil.java
@@ -20,6 +20,12 @@ import java.util.stream.IntStream;
  * @author webarata3
  */
 public class BenrippoiUtil {
+    /**
+     * Excelファイルを読み込みます
+     *
+     * @param fileName Excelファイル名。拡張子で読み込むフォーマットが決まります
+     * @return Excel Workbook
+     */
     public static Optional<Workbook> open(String fileName) {
         try {
             InputStream is = Files.newInputStream(Paths.get(fileName));
@@ -29,6 +35,12 @@ public class BenrippoiUtil {
         }
     }
 
+    /**
+     * Excelファイルを読み込みます
+     *
+     * @param is ExcelファイルのInputStream
+     * @return Excel Workbook
+     */
     public static Optional<Workbook> open(InputStream is) {
         Objects.requireNonNull(is, "InputStreamにnullは許可されていません");
         try (Workbook wb = WorkbookFactory.create(is)) {
@@ -38,6 +50,13 @@ public class BenrippoiUtil {
         }
     }
 
+    /**
+     * Excelのセルのインデックスをセルのラベル（A1、B2）に変更します。
+     *
+     * @param x 列番号（0〜）
+     * @param y 行番号（0〜）
+     * @return セルのラベル
+     */
     public static String cellIndexToCellLabel(int x, int y) {
         String cellName = dec26(x, 0);
         return cellName + (y + 1);
@@ -47,6 +66,13 @@ public class BenrippoiUtil {
         return (num > 25 ? dec26(num / 26, 1) : "") + String.valueOf((char) ('A' + (num - first) % 26));
     }
 
+    /**
+     * セルのラベル（A1、B2）のセルの取得
+     *
+     * @param sheet     シート
+     * @param cellLabel セルのラベル（A1、B2）
+     * @return Cell
+     */
     public static Cell getCell(Sheet sheet, String cellLabel) {
         Pattern p1 = Pattern.compile("([a-zA-Z]+)([0-9]+)");
         Matcher matcher = p1.matcher(cellLabel);
@@ -61,14 +87,29 @@ public class BenrippoiUtil {
         return getCell(sheet, x, Integer.parseInt(matcher.group(2)) - 1);
     }
 
-    public static Row getRow(Sheet sheet, int n) {
-        Row row = sheet.getRow(n);
+    /**
+     * Row（行）の取得
+     *
+     * @param sheet シート
+     * @param y     行番号（0〜）
+     * @return Row（行）
+     */
+    public static Row getRow(Sheet sheet, int y) {
+        Row row = sheet.getRow(y);
         if (row != null) {
             return row;
         }
-        return sheet.createRow(n);
+        return sheet.createRow(y);
     }
 
+    /**
+     * Cellの取得
+     *
+     * @param sheet シート
+     * @param x     列番号（0〜）
+     * @param y     行番号（0〜）
+     * @return Cell
+     */
     public static Cell getCell(Sheet sheet, int x, int y) {
         Row row = sheet.getRow(y);
         Cell cell = row.getCell(x);
@@ -78,6 +119,12 @@ public class BenrippoiUtil {
         return row.createCell(x, CellType.BLANK);
     }
 
+    /**
+     * 数値の正規化
+     *
+     * @param numeric 正規化する数値
+     * @return 正規化した数値
+     */
     public static String normalizeNumericString(double numeric) {
         // 44.0のような数値を44として取得するために、入力された数値と小数点以下を切り捨てた数値が
         // 一致した場合には、intにキャストして、小数点以下が表示されないようにしている
@@ -87,6 +134,12 @@ public class BenrippoiUtil {
         return String.valueOf(numeric);
     }
 
+    /**
+     * 計算式のセルの値の取得
+     *
+     * @param cell 計算式があるセル
+     * @return CellValue
+     */
     public static CellValue getFomulaCellValue(Cell cell) {
         Workbook wb = cell.getSheet().getWorkbook();
         CreationHelper helper = wb.getCreationHelper();
@@ -94,6 +147,12 @@ public class BenrippoiUtil {
         return evaluator.evaluate(cell);
     }
 
+    /**
+     * セルの値をString型で取得する
+     *
+     * @param cell セル
+     * @return String型の値
+     */
     public static String cellToString(Cell cell) {
         switch (cell.getCellTypeEnum()) {
             case STRING:
@@ -131,6 +190,12 @@ public class BenrippoiUtil {
         }
     }
 
+    /**
+     * セルの値をint型で取得する。小数は切り捨てられる
+     *
+     * @param cell セル
+     * @return int型の値
+     */
     public static int cellToInt(Cell cell) {
         switch (cell.getCellTypeEnum()) {
             case STRING:
@@ -160,15 +225,21 @@ public class BenrippoiUtil {
         }
     }
 
+    /**
+     * セルの値をdouble型で取得する。
+     *
+     * @param cell セル
+     * @return double型の値
+     */
     public static double cellToDouble(Cell cell) {
-        switch(cell.getCellTypeEnum()) {
+        switch (cell.getCellTypeEnum()) {
             case STRING:
                 return stringToDouble(cell.getStringCellValue());
             case NUMERIC:
                 return cell.getNumericCellValue();
             case FORMULA:
                 CellValue cellValue = getFomulaCellValue(cell);
-                switch(cellValue.getCellTypeEnum()) {
+                switch (cellValue.getCellTypeEnum()) {
                     case STRING:
                         return stringToDouble(cell.getStringCellValue());
                     case NUMERIC:
