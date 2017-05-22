82e7069c819b1e319794c0ac268b4c41b1fcb13a
webarata3 <webarata3@gmail.com>
2017-05-23 04:17:32 +0900

    エラーセルの取得テスト

--- src/test/java/link/webarata3/poi/CellProxyTest.java
+++ src/test/java/link/webarata3/poi/CellProxyTest.java
@@ -8,6 +8,7 @@ import org.junit.experimental.runners.Enclosed;
 import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
+import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 
@@ -66,6 +67,50 @@ public class CellProxyTest {
 
             CellProxy cellProxy = new CellProxy(cell);
             assertThat(cellProxy.toStr(), is(fixture.expected));
+        }
+    }
+
+    @RunWith(Theories.class)
+    public static class 異常系_toStr {
+        @Rule
+        public TemporaryFolder tempFolder = new TemporaryFolder();
+        @Rule
+        public ExpectedException thrown = ExpectedException.none();
+
+        @DataPoints
+        public static Fixture[] PARAMs = {
+            new Fixture("K2")
+        };
+
+        static class Fixture {
+            String cellLabel;
+
+            Fixture(String cellLabel) {
+                this.cellLabel = cellLabel;
+            }
+
+            @Override
+            public String toString() {
+                return "Fixture{" +
+                    "cellLabel='" + cellLabel + '\'' +
+                    '}';
+            }
+        }
+
+        @Theory
+        public void test(Fixture fixture) throws Exception {
+            Workbook wb = TestUtil.getTempWorkbook(tempFolder, "book1.xlsx");
+            assertThat(wb, is(notNullValue()));
+
+            Sheet sheet = wb.getSheetAt(0);
+            assertThat(sheet, is(notNullValue()));
+
+            Cell cell = BenrippoiUtil.getCell(sheet, fixture.cellLabel);
+            assertThat(fixture.toString(), cell, is(notNullValue()));
+
+            CellProxy cellProxy = new CellProxy(cell);
+            thrown.expect(PoiIllegalAccessException.class);
+            cellProxy.toStr();
         }
     }
 }
