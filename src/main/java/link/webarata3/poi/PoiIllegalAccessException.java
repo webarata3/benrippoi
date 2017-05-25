cb29b89b8dd6274993ecadd701edb2489581d115
webarata3 <webarata3@gmail.com>
2017-05-25 16:34:06 +0900

    不要なコンストラクタの削除

--- src/main/java/link/webarata3/poi/PoiIllegalAccessException.java
+++ src/main/java/link/webarata3/poi/PoiIllegalAccessException.java
@@ -1,22 +1,7 @@
 package link.webarata3.poi;
 
 public class PoiIllegalAccessException extends RuntimeException {
-    public PoiIllegalAccessException() {
-    }
-
     public PoiIllegalAccessException(String message) {
         super(message);
-    }
-
-    public PoiIllegalAccessException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public PoiIllegalAccessException(Throwable cause) {
-        super(cause);
-    }
-
-    public PoiIllegalAccessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
