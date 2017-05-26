d4b6daf60f3471054a5f81f0d9f6536b9f0f1e65
webarata3 <webarata3@gmail.com>
2017-05-27 03:02:32 +0900

    BenriWorkbookFactoryの追加

---
+++ src/main/java/link/webarata3/poi/BenriWorkbookFactory.java
@@ -0,0 +1,22 @@
+package link.webarata3.poi;
+
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class BenriWorkbookFactory {
+    public static BenriWorkbook create(String fileName) throws IOException, InvalidFormatException {
+        InputStream is = Files.newInputStream(Paths.get(fileName));
+        return create(is);
+    }
+
+    public static BenriWorkbook create(InputStream is) throws IOException, InvalidFormatException {
+        Workbook wb = BenrippoiUtil.open(is);
+        return new BenriWorkbook(wb);
+    }
+}
