d84e27b3c83a630370d8b199dc23eed77b347020
webarata3 <webarata3@gmail.com>
2017-05-27 02:08:22 +0900

    BenriCellのテストの追加

--- src/main/java/link/webarata3/poi/BenriCell.java
+++ src/main/java/link/webarata3/poi/BenriCell.java
@@ -3,6 +3,10 @@ package link.webarata3.poi;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Sheet;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class BenriCell {
     private Cell cell;
 
@@ -14,11 +18,31 @@ public class BenriCell {
         cell = BenrippoiUtil.getCell(sheet, cellLabel);
     }
 
+    public String toStr() {
+        return BenrippoiUtil.cellToString(cell);
+    }
+
     public int toInt() {
         return BenrippoiUtil.cellToInt(cell);
     }
 
-    public String toStr() {
-        return BenrippoiUtil.cellToString(cell);
+    public double toDouble() {
+        return BenrippoiUtil.cellToDouble(cell);
+    }
+
+    public boolean toBoolean() {
+        return BenrippoiUtil.cellToBoolean(cell);
+    }
+
+    public LocalDate toLocalDate() {
+        return BenrippoiUtil.cellToLocalDate(cell);
+    }
+
+    public LocalTime toLocalTime() {
+        return BenrippoiUtil.cellToLocalTime(cell);
+    }
+
+    public LocalDateTime toLocalDateTime() {
+        return BenrippoiUtil.cellToLocalDateTime(cell);
     }
 }
---
+++ src/test/java/link/webarata3/poi/BenriCellTest.java
@@ -0,0 +1,70 @@
+package link.webarata3.poi;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+public class BenriCellTest {
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    public void 正常系_toStr() throws Exception {
+        Workbook wb = TestUtil.getTempWorkbook(tempFolder, "book1.xlsx");
+        try (BenriWorkbook wbb = new BenriWorkbook(wb)) {
+            String actual = wbb.sheet("Sheet1").cell("B2").toStr();
+            assertThat(actual, is("あいうえお"));
+        }
+    }
+
+    public void 正常系_toInt() throws Exception {
+        Workbook wb = TestUtil.getTempWorkbook(tempFolder, "book1.xlsx");
+        try (BenriWorkbook wbb = new BenriWorkbook(wb)) {
+            int actual = wbb.sheet("Sheet1").cell("C2").toInt();
+            assertThat(actual, is(123));
+        }
+    }
+
+    public void 正常系_toDouble() throws Exception {
+        Workbook wb = TestUtil.getTempWorkbook(tempFolder, "book1.xlsx");
+        try (BenriWorkbook wbb = new BenriWorkbook(wb)) {
+            double actual = wbb.sheet("Sheet1").cell("D2").toDouble();
+            assertThat(actual, is(closeTo(150.15, 0.000001)));
+        }
+    }
+
+    public void 正常系_toBoolean() throws Exception {
+        Workbook wb = TestUtil.getTempWorkbook(tempFolder, "book1.xlsx");
+        try (BenriWorkbook wbb = new BenriWorkbook(wb)) {
+            boolean actual = wbb.sheet("Sheet1").cell("E2").toBoolean();
+            assertThat(actual, is(true));
+        }
+    }
+    public void 正常系_toLocalDate() throws Exception {
+        Workbook wb = TestUtil.getTempWorkbook(tempFolder, "book1.xlsx");
+        try (BenriWorkbook wbb = new BenriWorkbook(wb)) {
+            LocalDate actual = wbb.sheet("Sheet1").cell("E6").toLocalDate();
+            assertThat(actual, is(true));
+        }
+    }
+    public void 正常系_toLocalTime() throws Exception {
+        Workbook wb = TestUtil.getTempWorkbook(tempFolder, "book1.xlsx");
+        try (BenriWorkbook wbb = new BenriWorkbook(wb)) {
+            LocalTime actual = wbb.sheet("Sheet1").cell("E7").toLocalTime();
+            assertThat(actual, is(true));
+        }
+    }
+    public void 正常系_toLocalDateTime() throws Exception {
+        Workbook wb = TestUtil.getTempWorkbook(tempFolder, "book1.xlsx");
+        try (BenriWorkbook wbb = new BenriWorkbook(wb)) {
+            LocalDateTime actual = wbb.sheet("Sheet1").cell("E8").toLocalDateTime();
+            assertThat(actual, is(true));
+        }
+    }
+}
