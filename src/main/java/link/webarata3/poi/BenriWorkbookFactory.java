96bc4dfcb80118cc2baeaa3fb1175149eb7c204c
webarata3 <webarata3@gmail.com>
2017-06-02 04:10:49 +0900

    BenriWorkbookFactoryのコメント追加

--- src/main/java/link/webarata3/poi/BenriWorkbookFactory.java
+++ src/main/java/link/webarata3/poi/BenriWorkbookFactory.java
@@ -10,17 +10,41 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+/**
+ * BenriWorkbookのファクトリー
+ */
 public class BenriWorkbookFactory {
+    /**
+     * ファイル名を指定してBenriWorkbookを作成します。
+     *
+     * @param fileName ファイル名（拡張子でファイルタイプを判定する）
+     * @return BenriWorkbook
+     * @throws IOException
+     * @throws InvalidFormatException
+     */
     public static BenriWorkbook create(String fileName) throws IOException, InvalidFormatException {
         InputStream is = Files.newInputStream(Paths.get(fileName));
         return create(is);
     }
 
+    /**
+     * InputStreamからBenriWorkbookを作成します。
+     *
+     * @param is InputStream
+     * @return BenriWorkbook
+     * @throws IOException
+     * @throws InvalidFormatException
+     */
     public static BenriWorkbook create(InputStream is) throws IOException, InvalidFormatException {
         Workbook wb = BenrippoiUtil.open(is);
         return new BenriWorkbook(wb);
     }
 
+    /**
+     * 新規BenriWorkbookの作成
+     *
+     * @return 新規BenriWorkbook
+     */
     public static BenriWorkbook createBlank() {
         Workbook wb = new XSSFWorkbook();
         return new BenriWorkbook(wb);
